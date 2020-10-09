51ed6a24a568e5db08c329d5cbf4096a6d3b3009
maximejournet54 <maxime.journet@free.fr>
2020-10-09 17:47:25 +0200

    started main

--- test/src/application/Main.java
+++ test/src/application/Main.java
@@ -3,16 +3,18 @@ package application;
 import java.net.URL;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-		URL fxmlURL=getClass().getResource("./fenetre.fxml");
+		URL fxmlURL=getClass().getResource("/ressources/fenetre.fxml");
 		FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
 		Node root = fxmlLoader.load();
 		Scene scene = new Scene((VBox) root, 600, 400);
@@ -21,7 +23,7 @@ public class Main extends Application {
 		primaryStage.setTitle("Ma première fenêtre JavaFX");
 		primaryStage.show();
 		} catch (Exception e) {
-		e.printStackTrace();
+			e.printStackTrace();
 		} 
 	}
 	
